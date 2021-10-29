96ab6f67ba119a053c97123ffea529be171d8696
Zh li <7t8h88gbyb@privaterelay.appleid.com>
2021-10-29 21:12:18 +0800

    3.0.4

--- SobotKit.framework/Headers/ZCKitInfo.h
+++ SobotKit.framework/Headers/ZCKitInfo.h
@@ -282,12 +282,14 @@
 
 /**
  *   商品卡片信息是否自动发送（转人工成功时，自动发送商品卡片信息）
- *   默认不发送
+ *   默认关闭  开启后默认一次有效会话内只发送一次
  **/
 @property (nonatomic,assign) BOOL isSendInfoCard;
 
 /*
-   是否每次都自动发送商品信息 默认一次  需要先开启 isSendInfoCard = yes
+   是否每次都自动发送商品信息 （转人工成功时，自动发送商品卡片信息）
+   需要先开启 isSendInfoCard = YES
+  （3.0.3版本新增）
  */
 @property (nonatomic,assign) BOOL isEveryTimeSendCard;
 
@@ -305,7 +307,9 @@
 /// 人工后，是否主动发送一条信息
 @property(nonatomic,assign) BOOL autoSendOrderMessage;
 
-// 是否每次都发送 订单信息，默认值发送一次， 开启后每次都发送 ，改参数需要先设置 autoSendOrderMessage = yes
+/**
+ * 是否每次都发送订单信息，需要先设置参数 autoSendOrderMessage = YES
+ */
 @property(nonatomic,assign) BOOL isEveryTimeAutoSend;
 
 ////////////////////////////////////////////////////////////////
